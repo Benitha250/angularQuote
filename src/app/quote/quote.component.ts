d718a071767bd54d923778c754ae52a26d1ef551
Benitha250 <benithauwase05@gmail.com>
2020-11-03 14:12:03 +0200

    add codes in quotes.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,36 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
-
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
   quotes: Quote[] = [
-    new Quote(1, 'Do not settle for less', 'Benitha Uwase', 'Benitha', new Date(2020,8,3), 0 , 0), 
-  
+    new Quote(1, 'Do not settle for less', 'Unknown', 'Benitha', new Date(2020,5,2), 0, 0),
+
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
-  }  
-
+  }
   deleteQuote(isComplete, index){
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].showDescription}?`)
-
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
       if (toDelete){
         this.quotes.splice(index,1)
       }
     }
   }
-
-
-  constructor() {
-   }
-
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  constructor() { }
   ngOnInit(): void {
   }
-
 }
