aa0a2332a753edfd9520e75209ec3b0dcfee75b0
Benitha250 <benithauwase05@gmail.com>
2020-11-03 08:40:17 +0200

    add a quote detail component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '@angular/compiler';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -9,7 +9,7 @@ import { Quote } from '@angular/compiler';
 export class QuoteComponent implements OnInit {
 
   quote: Quote[] = [
-    new Quote(1, 'Education is the key to successs', 'Nelson mandela', 'Brendah', new Date(2020,8,3), 0 , 0), 
+    new Quote(1, 'Do not settle for less', 'Benitha Uwase', 'Benitha', new Date(2020,8,3), 0 , 0), 
   
   ];
   constructor() {
