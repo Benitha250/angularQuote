23aa72e3aff23763082157d21428d5df9ffc7b4d
Benitha250 <benithauwase05@gmail.com>
2020-11-05 08:08:59 +0200

    add highlight directive and other changes

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+constructor(){
+
+}
+//constructor(elem:ElementRef) {
+    //elem.nativeElement.style.backgroundColor="#834c69"
+   //}
+}
